c6ed7aad260575e0c1fe83787fd535611ad69424
Adriana de Lazzari <adrianaclazzari@gmail.com>
2022-08-16 14:04:42 -0400

    Adding search for city functionality

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "35a3cd2406msh128b479d0ccc58ap126fc8jsn38d9aeef6508",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
